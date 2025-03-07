5ca45dd60087619d31fea217724b260a49285799
venky916 <venkateshsmsv1999@gmail.com>
2025-03-07 20:15:38 +0530

    cors fix

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://dev-tinder-web-delta.vercel.app/",
+      "https://dev-tinder-web-delta.vercel.app",
     ],
     credentials: true,
   })
